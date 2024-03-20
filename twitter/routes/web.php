b169c1e46ad4b62c8c1890e52b92a94fe14c65db
Loyd Ngei <loydngei@gmail.com>
2024-03-20 13:29:54 +0300

    Added get Routes

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/feed', function () {
+    return view('feed');
+});
+
+
+Route::get('/profile', function () {
+    return view('user.profile');
+});
